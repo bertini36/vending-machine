ff639f3a1d5b8e69fac5a20a0c2cb30f70f43349
bertini36 <albertopouquiros@gmail.com>
2023-08-04 08:21:01 +0200

    fix responsive

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -21,17 +21,23 @@ export const Product = ({ title, price, color, photo }: ProductProps) => {
 			paddingTop: '5%',
 			paddingBottom: '3%',
 			borderRadius: '7px',
-			height: '250px',
+			height: '240px',
 			cursor: 'pointer',
 			'&:hover': {
 				border: '2px solid',
 				borderColor: color,
 			},
 		}}>
-			<Box sx={{ fontSize: 21, fontWeight: 800, color: color }}>{ title }</Box>
-			<Box sx={{ marginTop: '4%', marginBottom: '4%' }}>
-				<img src={photo} style={{ width: '50%', height: '50%', alignSelf: 'center' }} alt={"logo"}/>
-			</Box>
+			<Box style={{ fontSize: 21, fontWeight: 800, color: color }}>{ title }</Box>
+			<img
+				src={photo}
+				style={{
+					width: '40%',
+					height: '60%',
+					alignSelf: 'center',
+					marginTop: '4%',
+					marginBottom: '4%'
+			}} alt={"logo"}/>
 			<Box style={{fontSize: '30px', fontStyle: 'bold', color: color}}>
 				<strong style={{ border: '2px solid', borderRadius: '30px', padding: 5}}>{ price }€</strong>
 			</Box>
